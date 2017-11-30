4cbedcf1eeb61324af1b7a4926e727698d06e6ba
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-30 00:51:30 -0500

    Update the ShoppingBasket class to ensure all objects passed into the constructor respond to isImported and isExempt

--- src/ShoppingBasket.ts
+++ src/ShoppingBasket.ts
@@ -1,7 +1,13 @@
 export class ShoppingBasket {
-    public shoppingBasket: Array<{ quantity: number, description: string, price: number }>;
+    public shoppingBasket: Array<{
+            quantity: number,
+            description: string,
+            price: number,
+            isImported: (description: string) => boolean,
+            isExempt: (description: string) => boolean
+    }>;
 
-    public constructor(...items: Array<{ quantity: number, description: string, price: number }>) {
+    public constructor(...items: Array<{quantity: number, description: string, price: number, isImported: (description: string) => boolean, isExempt: (description: string) => boolean}>) {
         this.shoppingBasket = items;
     }
 }
