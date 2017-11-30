7994909d6649cfbe190b6008bb7bbc66e37977dd
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-29 22:59:36 -0500

    Add a print method in the Receipt class that will ultimately print the final receipt to the console Add a unit test for the print method

--- src/Receipt.ts
+++ src/Receipt.ts
@@ -6,4 +6,15 @@ export class Receipt {
     public constructor(shoppingBasket: ShoppingBasket) {
         this.itemList = shoppingBasket;
     }
+
+    public print(): string {
+        let output = "";
+        const basket = this.itemList.shoppingBasket;
+
+        for (const item of basket) {
+            output += `${item.quantity} ${item.description}: ${item.price}\n`;
+        }
+
+        return output;
+    }
 }
--- test/Receipt.spec.ts
+++ test/Receipt.spec.ts
@@ -4,5 +4,18 @@ import { Receipt } from "../src/Receipt";
 import { ShoppingBasket } from "../src/ShoppingBasket";
 
 describe("Receipt", () => {
-    // Update
+    describe("print", () => {
+        it("should print the contents of a given shopping basket", () => {
+            const item1: Item = new Item(1, "book", 12.49);
+            const item2: Item = new Item(1, "music CD", 16.49);
+            const item3: Item = new Item(1, "chocolate bar", 0.85);
+
+            const shoppingbasket: ShoppingBasket = new ShoppingBasket(item1, item2, item3);
+            const receipt: Receipt = new Receipt(shoppingbasket);
+
+            const expectedOutput = `${item1.quantity} ${item1.description}: ${item1.price}\n${item2.quantity} ${item2.description}: ${item2.price}\n${item3.quantity} ${item3.description}: ${item3.price}\n`;
+
+            expect(receipt.print()).to.equal(expectedOutput);
+        });
+    });
 });
