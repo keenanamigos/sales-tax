d14f55edab7ce6e4535a577e738472d3dbcbf8a8
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-30 13:00:59 -0500

    Add private validateValues and validateItems functions to the ShoppingBasket class. We only want items with a quantity greater than zero, a description that is not whitspace or an empty string, and a price greater than zero included in the shopping basket

--- src/ShoppingBasket.ts
+++ src/ShoppingBasket.ts
@@ -1,9 +1,32 @@
+import { NumberHelper } from "./helpers/NumberHelper";
 import { IItem } from "./interfaces/IItem";
 
 export class ShoppingBasket {
     public shoppingBasket: IItem[];
 
     public constructor(...items: IItem[]) {
-        this.shoppingBasket = items;
+        this.shoppingBasket = this.validateItems(items);
+    }
+
+    private validateItems(items: IItem[]): IItem[] {
+        let index = 0;
+
+        for (const item of items) {
+            if (!(this.validateValues(item))) {
+                items.splice(index, 1);
+            }
+
+            index++;
+        }
+
+        return items;
+    }
+
+    private validateValues(item: IItem): boolean {
+        if (NumberHelper.isGreaterThanZero(item.quantity) && item.description && NumberHelper.isGreaterThanZero(item.price)) {
+            return true;
+        }
+
+        return false;
     }
 }
