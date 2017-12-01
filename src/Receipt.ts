588be2439168058047e4c33b6e8dc9e1008ad141
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-30 21:23:53 -0500

    Extract functions out from the Receipt class into the NumberHelper and TaxCalculator classes respectively and update the references, move the Tax enums into an enums directory

--- src/Receipt.ts
+++ src/Receipt.ts
@@ -1,6 +1,7 @@
+import { NumberHelper } from "./helpers/NumberHelper";
 import { IItem } from "./interfaces/IItem";
 import { ShoppingBasket } from "./ShoppingBasket";
-import { Tax } from "./Tax";
+import { TaxCalculator } from "./TaxCalculator";
 
 export class Receipt {
     public itemList: ShoppingBasket;
@@ -18,38 +19,18 @@ export class Receipt {
         const preTaxTotal = this.getTotal(basket);
 
         for (const item of basket) {
-            this.applyTax(item);
+            TaxCalculator.applyTax(item);
             formattedPrice = item.price.toFixed(2);
             output += `${item.quantity} ${item.description}: ${formattedPrice}\n`;
         }
 
         postTaxTotal = this.getTotal(basket);
-        salesTax = this.getSalesTax(preTaxTotal, postTaxTotal);
+        salesTax = TaxCalculator.getSalesTax(preTaxTotal, postTaxTotal);
 
         output += `\nSales Taxes: ${salesTax.toFixed(2)}`;
         output += `\nTotal: ${postTaxTotal}`;
 
         return output;
-    }
-
-    // Extract into TaxCalculator class as a static method
-    private applyTax(item: IItem): void {
-        /* Tax scenarios */
-        if (item.isImported(item.description) && !item.isExempt(item.description)) {
-            item.price += (item.price * Tax.SalesTax) + (item.price * Tax.ImportTax);
-            item.price = this.roundUp(item.price);
-        } else if (!item.isImported(item.description) && !item.isExempt(item.description)) {
-            item.price += (item.price * Tax.SalesTax);
-            item.price = this.roundUp(item.price);
-        } else if (item.isImported(item.description) && item.isExempt(item.description)) {
-            item.price += (item.price * Tax.ImportTax);
-            item.price = this.roundUp(item.price);
-        }
-    }
-
-    private roundUp(price: number): number {
-        // Rounding up to the nearest 0.05
-        return (Math.ceil(price * 20) / 20);
     }
 
     private getTotal(basket: IItem[]): number {
@@ -59,11 +40,6 @@ export class Receipt {
             total += item.price;
         }
 
-        return this.roundUp(total);
-    }
-
-    // Extract into TaxCalculator class as a static method
-    private getSalesTax(preTaxTotal: number, postTaxTotal: number): number {
-        return (postTaxTotal - preTaxTotal);
+        return NumberHelper.roundUp(total);
     }
 }
---
+++ src/TaxCalculator.ts
@@ -0,0 +1,23 @@
+import { Tax } from "./enums/Tax";
+import { NumberHelper } from "./helpers/NumberHelper";
+import { IItem } from "./interfaces/IItem";
+
+export class TaxCalculator {
+    public static applyTax(item: IItem): void {
+        /* Tax scenarios */
+        if (item.isImported(item.description) && !item.isExempt(item.description)) {
+            item.price += (item.price * Tax.SalesTax) + (item.price * Tax.ImportTax);
+            item.price = NumberHelper.roundUp(item.price);
+        } else if (!item.isImported(item.description) && !item.isExempt(item.description)) {
+            item.price += (item.price * Tax.SalesTax);
+            item.price = NumberHelper.roundUp(item.price);
+        } else if (item.isImported(item.description) && item.isExempt(item.description)) {
+            item.price += (item.price * Tax.ImportTax);
+            item.price = NumberHelper.roundUp(item.price);
+        }
+    }
+
+    public static getSalesTax(preTaxTotal: number, postTaxTotal: number): number {
+        return (postTaxTotal - preTaxTotal);
+    }
+}
