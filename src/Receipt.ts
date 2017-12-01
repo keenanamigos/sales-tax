047c1da3c9647b254d5dafebd01e9ae089c7ab2a
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-30 19:30:31 -0500

    Update print function in the Receipt class to print the prices with taxes added, the sales tax value, and the total value Add private applyTax, roundUp, getTotal, and getSalesTax methods. Some of these will be extracted out into a separate class as the Receipt class has too much responsibility currently
    Update unit tests to reflect output string. Not currently passing because of small rounding error

--- src/Receipt.ts
+++ src/Receipt.ts
@@ -1,4 +1,6 @@
+import { IItem } from "./interfaces/IItem";
 import { ShoppingBasket } from "./ShoppingBasket";
+import { Tax } from "./Tax";
 
 export class Receipt {
     public itemList: ShoppingBasket;
@@ -9,12 +11,59 @@ export class Receipt {
 
     public print(): string {
         let output = "";
+        let postTaxTotal = 0;
+        let salesTax: number;
+        let formattedPrice: string;
         const basket = this.itemList.shoppingBasket;
+        const preTaxTotal = this.getTotal(basket);
 
         for (const item of basket) {
-            output += `${item.quantity} ${item.description}: ${item.price}\n`;
+            this.applyTax(item);
+            formattedPrice = item.price.toFixed(2);
+            output += `${item.quantity} ${item.description}: ${formattedPrice}\n`;
         }
 
+        postTaxTotal = this.getTotal(basket);
+        salesTax = this.getSalesTax(preTaxTotal, postTaxTotal);
+
+        output += `\nSales Taxes: ${salesTax.toFixed(2)}`;
+        output += `\nTotal: ${postTaxTotal}`;
+
         return output;
+    }
+
+    // Extract into TaxCalculator class as a static method
+    private applyTax(item: IItem): void {
+        /* Tax scenarios */
+        if (item.isImported(item.description) && !item.isExempt(item.description)) {
+            item.price += (item.price * Tax.SalesTax) + (item.price * Tax.ImportTax);
+            item.price = this.roundUp(item.price);
+        } else if (!item.isImported(item.description) && !item.isExempt(item.description)) {
+            item.price += (item.price * Tax.SalesTax);
+            item.price = this.roundUp(item.price);
+        } else if (item.isImported(item.description) && item.isExempt(item.description)) {
+            item.price += (item.price * Tax.ImportTax);
+            item.price = this.roundUp(item.price);
+        }
+    }
+
+    private roundUp(price: number): number {
+        // Rounding up to the nearest 0.05
+        return (Math.ceil(price * 20) / 20);
+    }
+
+    private getTotal(basket: IItem[]): number {
+        let total = 0;
+
+        for (const item of basket) {
+            total += item.price;
+        }
+
+        return this.roundUp(total);
+    }
+
+    // Extract into TaxCalculator class as a static method
+    private getSalesTax(preTaxTotal: number, postTaxTotal: number): number {
+        return (postTaxTotal - preTaxTotal);
     }
 }
--- test/Receipt.spec.ts
+++ test/Receipt.spec.ts
@@ -7,13 +7,12 @@ describe("Receipt", () => {
     describe("print", () => {
         it("should print the contents of a given shopping basket", () => {
             const item1: StoreItem = new StoreItem(1, "book", 12.49);
-            const item2: StoreItem = new StoreItem(1, "music CD", 16.49);
+            const item2: StoreItem = new StoreItem(1, "music CD", 14.99);
             const item3: StoreItem = new StoreItem(1, "chocolate bar", 0.85);
 
             const shoppingbasket: ShoppingBasket = new ShoppingBasket(item1, item2, item3);
             const receipt: Receipt = new Receipt(shoppingbasket);
-
-            const expectedOutput = `${item1.quantity} ${item1.description}: ${item1.price}\n${item2.quantity} ${item2.description}: ${item2.price}\n${item3.quantity} ${item3.description}: ${item3.price}\n`;
+            const expectedOutput = `1 book: 12.49\n1 music CD: 16.49\n1 chocolate bar: 0.85\nSales Taxes: 1.50\nTotal: 29.83`;
 
             expect(receipt.print()).to.equal(expectedOutput);
         });
