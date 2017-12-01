329bde359cd47e903105281a0a4973b25f76b717
Keenan Johnson <keenan.johnson1@outlook.com>
2017-12-01 13:45:45 -0500

    Create a list of items, a shopping basket, and a receipt that prints the proper information to the console

--- app.ts
+++ app.ts
@@ -2,5 +2,10 @@
 import * as chalk from "chalk";
 import { Receipt } from "./src/Receipt";
 import { ShoppingBasket } from "./src/ShoppingBasket";
-import { StoreItem } from "./src/StoreItem";
 import { TextFileParser } from "./src/TextFileParser";
+
+const arrayOfItems = TextFileParser.parse();
+const shoppingBasket = new ShoppingBasket(...arrayOfItems);
+const receipt = new Receipt(shoppingBasket);
+
+console.log(receipt.print());
