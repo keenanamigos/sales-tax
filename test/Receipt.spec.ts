aca4537f3f99d88e2b1ea30650995aa10ff0d108
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-30 23:03:24 -0500

    Update it function text in Receipt unit test

--- test/Receipt.spec.ts
+++ test/Receipt.spec.ts
@@ -5,7 +5,7 @@ import { StoreItem } from "../src/StoreItem";
 
 describe("Receipt", () => {
     describe("print", () => {
-        it("should print the contents of a given shopping basket", () => {
+        it("should print the contents of a given shopping basket in the proper format", () => {
             const item1: StoreItem = new StoreItem(1, "book", 12.49);
             const item2: StoreItem = new StoreItem(1, "music CD", 14.99);
             const item3: StoreItem = new StoreItem(1, "chocolate bar", 0.85);
