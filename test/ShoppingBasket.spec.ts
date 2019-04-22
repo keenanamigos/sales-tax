446df65d6fbe8b69bc8531a79021e75bc7b8857c
Keenan Johnson <keenan.johnson1@outlook.com>
2019-04-22 00:02:46 -0400

    Add unit tests for the ShoppingBasket class

--- test/ShoppingBasket.spec.ts
+++ test/ShoppingBasket.spec.ts
@@ -3,5 +3,36 @@ import { ShoppingBasket } from "../src/ShoppingBasket";
 import { StoreItem } from "../src/StoreItem";
 
 describe("ShoppingBasket", () => {
-    // Update
+    describe("validateItems", () => {
+        it("should return all valid items", () => {
+            const storeItems: StoreItem[] = [
+                new StoreItem(1, "book", 12.49),
+                new StoreItem(1, "imported box of chocolates", 10),
+                new StoreItem(1, "imported bottle of perfume", 27.99)
+            ];
+            const shoppingBasket = new ShoppingBasket(...storeItems);
+            // tslint:disable-next-line:no-string-literal
+            expect(shoppingBasket["validateItems"](storeItems).length).to.equal(3);
+        });
+    });
+
+    describe("validateValues", () => {
+        const storeItems: StoreItem[] = [
+            new StoreItem(1, "book", 12.49),
+            new StoreItem(1, "imported box of chocolates", 10),
+            new StoreItem(1, "imported bottle of perfume", 27.99)
+        ];
+        it("should return true if the item contains valid values", () => {
+            const shoppingBasket = new ShoppingBasket(...storeItems);
+            // tslint:disable-next-line:no-string-literal
+            expect(shoppingBasket["validateValues"](storeItems[0])).to.equal(true);
+        });
+
+        it("should return false if the item contains invalid values", () => {
+            const invalidItem = new StoreItem(1, "", 10);
+            const shoppingBasket = new ShoppingBasket(...storeItems);
+            // tslint:disable-next-line:no-string-literal
+            expect(shoppingBasket["validateValues"](invalidItem)).to.equal(false);
+        });
+    });
 });
